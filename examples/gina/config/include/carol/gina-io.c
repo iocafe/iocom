57a3eb001b5be5644f03e9628047516736f7bac9
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2019-11-09 03:38:38 -0500

    xx

--- examples/gina/config/include/carol/gina-io.c
+++ examples/gina/config/include/carol/gina-io.c
@@ -8,106 +8,6 @@ static os_short pins_inputs_touch_sensor_prm[]= {PIN_RV, PIN_RV, PIN_TOUCH, 1};
 
 /* Parameters for outputs */
 static os_short pins_outputs_led_builtin_prm[]= {PIN_RV, PIN_RV};
-
-/* Parameters for analog_inputs */
-static os_short pins_analog_inputs_potentiometer_prm[]= {PIN_RV, PIN_RV, PIN_DELAY, 11, PIN_SPEED, 3};
-
-/* Parameters for pwm */
-static os_short pins_pwm_servo_prm[]= {PIN_RV, PIN_RV, PIN_RESOLUTION, 12, PIN_FREQENCY, 50, PIN_INIT, 2048};
-static os_short pins_pwm_dimmer_led_prm[]= {PIN_RV, PIN_RV, PIN_RESOLUTION, 12, PIN_FREQENCY, 5000, PIN_INIT, 0};
-
-/* JANE IO configuration structure */
-const pins_t pins =
-{
-  {{3, &pins.inputs.dip_switch_3}, /* inputs */
-    {PIN_INPUT, 0, 34, pins_inputs_dip_switch_3_prm, sizeof(pins_inputs_dip_switch_3_prm)/sizeof(os_short), OS_NULL, &gina.exp.dip_switch_3}, /* dip_switch_3 */
-    {PIN_INPUT, 0, 35, pins_inputs_dip_switch_4_prm, sizeof(pins_inputs_dip_switch_4_prm)/sizeof(os_short), OS_NULL, &gina.exp.dip_switch_4}, /* dip_switch_4 */
-    {PIN_INPUT, 0, 4, pins_inputs_touch_sensor_prm, sizeof(pins_inputs_touch_sensor_prm)/sizeof(os_short), OS_NULL, &gina.exp.touch_sensor} /* touch_sensor */
-  },
-
-  {{1, &pins.outputs.led_builtin}, /* outputs */
-    {PIN_OUTPUT, 0, 2, pins_outputs_led_builtin_prm, sizeof(pins_outputs_led_builtin_prm)/sizeof(os_short), OS_NULL, &gina.imp.led_builtin} /* led_builtin */
-  },
-
-  {{1, &pins.analog_inputs.potentiometer}, /* analog_inputs */
-    {PIN_ANALOG_INPUT, 0, 25, pins_analog_inputs_potentiometer_prm, sizeof(pins_analog_inputs_potentiometer_prm)/sizeof(os_short), OS_NULL, &gina.exp.potentiometer} /* potentiometer */
-  },
-
-  {{2, &pins.pwm.servo}, /* pwm */
-    {PIN_PWM, 0, 32, pins_pwm_servo_prm, sizeof(pins_pwm_servo_prm)/sizeof(os_short), OS_NULL, &gina.imp.servo}, /* servo */
-    {PIN_PWM, 1, 33, pins_pwm_dimmer_led_prm, sizeof(pins_pwm_dimmer_led_prm)/sizeof(os_short), OS_NULL, &gina.imp.dimmer_led} /* dimmer_led */
-  }
-};
-
-/* List of pin type groups */
-static const PinGroupHdr *pins_group_list[] =
-{
-  &pins.inputs.hdr,
-  &pins.outputs.hdr,
-  &pins.analog_inputs.hdr,
-  &pins.pwm.hdr
-};
-
-/* JANE IO configuration top header structure */
-const IoPinsHdr pins_hdr = {pins_group_list, sizeof(pins_group_list)/sizeof(PinGroupHdr*)};
-
-/* Parameters for inputs */
-static os_short pins_inputs_dip_switch_3_prm[]= {PIN_RV, PIN_RV, PIN_PULL_UP, 1};
-static os_short pins_inputs_dip_switch_4_prm[]= {PIN_RV, PIN_RV};
-static os_short pins_inputs_touch_sensor_prm[]= {PIN_RV, PIN_RV, PIN_TOUCH, 1};
-
-/* Parameters for outputs */
-static os_short pins_outputs_led_builtin_prm[]= {PIN_RV, PIN_RV};
-
-/* Parameters for analog_inputs */
-static os_short pins_analog_inputs_potentiometer_prm[]= {PIN_RV, PIN_RV, PIN_DELAY, 11, PIN_SPEED, 3};
-
-/* Parameters for pwm */
-static os_short pins_pwm_servo_prm[]= {PIN_RV, PIN_RV, PIN_RESOLUTION, 12, PIN_FREQENCY, 50, PIN_INIT, 2048};
-static os_short pins_pwm_dimmer_led_prm[]= {PIN_RV, PIN_RV, PIN_RESOLUTION, 12, PIN_FREQENCY, 5000, PIN_INIT, 0};
-
-/* JANE IO configuration structure */
-const pins_t pins =
-{
-  {{3, &pins.inputs.dip_switch_3}, /* inputs */
-    {PIN_INPUT, 0, 34, pins_inputs_dip_switch_3_prm, sizeof(pins_inputs_dip_switch_3_prm)/sizeof(os_short), OS_NULL, &gina.exp.dip_switch_3}, /* dip_switch_3 */
-    {PIN_INPUT, 0, 35, pins_inputs_dip_switch_4_prm, sizeof(pins_inputs_dip_switch_4_prm)/sizeof(os_short), OS_NULL, &gina.exp.dip_switch_4}, /* dip_switch_4 */
-    {PIN_INPUT, 0, 4, pins_inputs_touch_sensor_prm, sizeof(pins_inputs_touch_sensor_prm)/sizeof(os_short), OS_NULL, &gina.exp.touch_sensor} /* touch_sensor */
-  },
-
-  {{1, &pins.outputs.led_builtin}, /* outputs */
-    {PIN_OUTPUT, 0, 2, pins_outputs_led_builtin_prm, sizeof(pins_outputs_led_builtin_prm)/sizeof(os_short), OS_NULL, &gina.imp.led_builtin} /* led_builtin */
-  },
-
-  {{1, &pins.analog_inputs.potentiometer}, /* analog_inputs */
-    {PIN_ANALOG_INPUT, 0, 25, pins_analog_inputs_potentiometer_prm, sizeof(pins_analog_inputs_potentiometer_prm)/sizeof(os_short), OS_NULL, &gina.exp.potentiometer} /* potentiometer */
-  },
-
-  {{2, &pins.pwm.servo}, /* pwm */
-    {PIN_PWM, 0, 32, pins_pwm_servo_prm, sizeof(pins_pwm_servo_prm)/sizeof(os_short), OS_NULL, &gina.imp.servo}, /* servo */
-    {PIN_PWM, 1, 33, pins_pwm_dimmer_led_prm, sizeof(pins_pwm_dimmer_led_prm)/sizeof(os_short), OS_NULL, &gina.imp.dimmer_led} /* dimmer_led */
-  }
-};
-
-/* List of pin type groups */
-static const PinGroupHdr *pins_group_list[] =
-{
-  &pins.inputs.hdr,
-  &pins.outputs.hdr,
-  &pins.analog_inputs.hdr,
-  &pins.pwm.hdr
-};
-
-/* JANE IO configuration top header structure */
-const IoPinsHdr pins_hdr = {pins_group_list, sizeof(pins_group_list)/sizeof(PinGroupHdr*)};
-
-/* Parameters for inputs */
-static os_short pins_inputs_dip_switch_3_prm[]= {PIN_RV, PIN_RV, PIN_PULL_UP, 1};
-static os_short pins_inputs_dip_switch_4_prm[]= {PIN_RV, PIN_RV};
-static os_short pins_inputs_touch_sensor_prm[]= {PIN_RV, PIN_RV, PIN_TOUCH, 1};
-
-/* Parameters for outputs */
-static os_short pins_outputs_led_builtin_prm[]= {PIN_RV, PIN_RV};
 static os_short pins_outputs_A_prm[]= {PIN_RV, PIN_RV};
 static os_short pins_outputs_B_prm[]= {PIN_RV, PIN_RV};
 static os_short pins_outputs_C_prm[]= {PIN_RV, PIN_RV};
@@ -121,8 +21,8 @@ static os_short pins_outputs_H_prm[]= {PIN_RV, PIN_RV};
 static os_short pins_analog_inputs_potentiometer_prm[]= {PIN_RV, PIN_RV, PIN_DELAY, 11, PIN_SPEED, 3};
 
 /* Parameters for pwm */
-static os_short pins_pwm_servo_prm[]= {PIN_RV, PIN_RV, PIN_RESOLUTION, 12, PIN_FREQENCY, 50, PIN_INIT, 2048};
-static os_short pins_pwm_dimmer_led_prm[]= {PIN_RV, PIN_RV, PIN_RESOLUTION, 12, PIN_FREQENCY, 5000, PIN_INIT, 0};
+static os_short pins_pwm_servo_prm[]= {PIN_RV, PIN_RV, PIN_INIT, 2048, PIN_FREQENCY, 50, PIN_RESOLUTION, 12};
+static os_short pins_pwm_dimmer_led_prm[]= {PIN_RV, PIN_RV, PIN_INIT, 0, PIN_FREQENCY, 5000, PIN_RESOLUTION, 12};
 
 /* GINA IO configuration structure */
 const pins_t pins =
