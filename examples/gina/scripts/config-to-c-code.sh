bf4dfe33ec2d1e859c809304ad2b33991604205a
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2019-11-08 21:31:20 -0500

    config-to-c scripts merged

--- examples/gina/scripts/config-to-c-code.sh
+++ examples/gina/scripts/config-to-c-code.sh
@@ -6,6 +6,8 @@ export MYCONFIG=${MYCODEROOT}/iocom/examples/${MYAPP}/config
 
 export MYHW=carol
 
+python3 ${MYCODEROOT}/pins/scripts/pins-to-c.py ${MYCONFIG}/pins/${MYHW}/${MYAPP}-io.json -o ${MYCONFIG}/include/${MYHW}/${MYAPP}-io.c -s ${MYCONFIG}/signals/${MYAPP}-signals.json
+
 ${MYCODEROOT}/bin/linux/json --t2b -title ${MYCONFIG}/signals/${MYAPP}-signals.json ${MYCONFIG}/signals/${MYAPP}-signals.binjson
 ${MYCODEROOT}/bin/linux/json --b2t  ${MYCONFIG}/signals/${MYAPP}-signals.binjson ${MYCONFIG}/signals/${MYAPP}-signals-check.json
 
