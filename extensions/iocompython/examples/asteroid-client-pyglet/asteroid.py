1b6c28029c0a61bcc0de37225b17912863578ea2
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2019-12-03 10:14:50 -0500

    xx

--- extensions/iocompython/examples/asteroid-client-pyglet/asteroid.py
+++ extensions/iocompython/examples/asteroid-client-pyglet/asteroid.py
@@ -17,8 +17,8 @@ data = json2bin(signal_conf)
 info = MemoryBlock(root, 'upward', 'info', nbytes=len(data))
 info.publish(data)
 # connection = Connection(root, "192.168.1.220", "socket,upward")
-connection = Connection(root, "127.0.0.1", "socket,upward")
-# connection = Connection(root, "3.135.236.95", "socket,upward")
+# connection = Connection(root, "127.0.0.1", "socket,upward")
+connection = Connection(root, "3.135.236.95", "socket,upward")
 
 # Setup Python access to exported IO signals. These pass user keyboard control to the asteroid service.
 exp_force_x = Signal(root, "force_x")
