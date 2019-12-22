2a9882c3197262a9911cd7ea32fe20c14184d30c
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2019-12-21 23:29:32 -0500

    xx

--- extensions/iocompython/examples/i-spy/ispy.py
+++ extensions/iocompython/examples/i-spy/ispy.py
@@ -21,6 +21,15 @@ from kivy.uix.button import Button
 from kivy.config import ConfigParser
 from kivy.uix.settings import SettingItem
 
+from kivy.core.window import Window
+from kivy.uix.popup import Popup
+from kivy.uix.boxlayout import BoxLayout
+from kivy.metrics import dp
+from kivy.uix.textinput import TextInput
+from kivy.uix.widget import Widget            
+
+from mblks import MblkSpy
+
 # We first define our GUI
 kv = '''
 BoxLayout:
@@ -42,7 +51,7 @@ json = '''
         "desc": "Listen for incoming connections or connect actively?",
         "section": "My Label",
         "key": "conf_role",
-        "options": ["LISTEN", "CONNECT"]
+        "options": ["SERVER", "CLIENT"]
     },
     {
         "type": "options",
@@ -69,16 +78,30 @@ json = '''
     {
         "type": "string",
         "title": "user name",
-        "desc": "Authenticate to the device as",
-        "section": "My Label",
+        "desc": "Authenticate to the device as (vieview is client)",
+        "section": "client",
         "key": "conf_user"
     },
     {
         "type": "string",
-        "title": "password",
-        "desc": "Password for authentication",
-        "section": "My Label",
-        "key": "conf_password"
+        "title": "certificate chain",
+        "desc": "Chain of trust, PEM certificate bundle file (client)",
+        "section": "client",
+        "key": "conf_cert_chain"
+    },
+    {
+        "type": "string",
+        "title": "server certificate",
+        "desc": "Certificate to present to clients (public)",
+        "section": "server",
+        "key": "conf_serv_cert"
+    },
+    {
+        "type": "string",
+        "title": "server key",
+        "desc": "Server's private key (seacret)",
+        "section": "server",
+        "key": "conf_serv_key"
     },
     {
         "type": "buttons",
@@ -95,36 +118,51 @@ class SettingButtons(SettingItem):
 
     def __init__(self, **kwargs):
         self.register_event_type('on_release')
+
         # For Python3 compatibility we need to drop the buttons keyword when calling super.
         kw = kwargs.copy()
         kw.pop('buttons', None)
         super(SettingItem, self).__init__(**kw)
+        
         for aButton in kwargs["buttons"]:
             oButton=Button(text=aButton['title'], font_size= '15sp')
             oButton.ID=aButton['id']
             self.add_widget(oButton)
             oButton.bind (on_release=self.On_ButtonPressed)
+        
+
     def set_value(self, section, key, value):
         # set_value normally reads the configparser values and runs on an error
         # to do nothing here
         return
+
     def On_ButtonPressed(self,instance):
         self.panel.settings.dispatch('on_config_change',self.panel.config, self.section, self.key, instance.ID)
 
 class MyApp(App):
     def build(self):
+        self.title = 'i-spy'
         self.mysettings = MySettingsWithSidebar()
         self.mysettings.register_type('buttons', SettingButtons)
         self.mysettings.app = self
 #        self.config.setdefaults('My Label', {'text': 'Hello', 'font_size': 20, 'optxt': 'A'})
 #        self.mysettings.add_json_panel('My Label', self.config, data=json)
         config = ConfigParser()
-        config.setdefaults('My Label', {'conf_role': 'LISTEN', 'conf_transport': 'TLS', 'conf_ip': '192.168.1.220', 'conf_serport': 'COM1', 'conf_user': 'administrator', 'conf_password': ''})
-        self.mysettings.add_json_panel('IO device', config, data=json)
+        config.setdefaults('My Label', {'conf_role': 'CLIENT', 'conf_transport': 'TLS', 'conf_ip': '192.168.1.220', 'conf_serport': 'COM1', 'conf_authentication': 'CLIENT'})
+        config.setdefaults('client', {'conf_user': 'administrator', 'conf_cert_chain': 'bob-bundle.crt'})
+        config.setdefaults('server', {'conf_serv_cert': 'alice.crt', 'conf_serv_key': 'alice.key'})
+        # config.adddefaultsection('client')
+        # config.setdefaults('client', {'conf_cert_chain': 'bob-bundle.crt'})
+        self.mysettings.add_json_panel('IO device connect', config, data=json)
+
+        mblks = MblkSpy()
+        mblks.setup_my_panel(self.mysettings)
+        
         return self.mysettings
         """
         Build and return the root widget.
         """
+        '''
         # The line below is optional. You could leave it out or use one of the
         # standard options, such as SettingsWithSidebar, SettingsWithSpinner
         # etc.
@@ -136,12 +174,15 @@ class MyApp(App):
         label.text = self.config.get('My Label', 'text')
         label.font_size = float(self.config.get('My Label', 'font_size'))
         return root
+        '''
 
+    '''
     def build_config(self, config):
         """
         Set the default values for the configs sections.
         """
         config.setdefaults('My Label', {'text': 'Hello', 'font_size': 20})
+    
 
     def build_settings(self, settings):
         """
@@ -152,6 +193,7 @@ class MyApp(App):
         #     settings.add_json_panel('My Label', self.config, 'settings.json')
         settings.add_json_panel('My Label', self.config, data=json)
 
+    
     def on_config_change(self, config, section, key, value):
         """
         Respond to changes in the configuration.
@@ -170,6 +212,7 @@ class MyApp(App):
         The settings panel has been closed.
         """
         Logger.info("main.py: App.close_settings: {0}".format(settings))
+    '''
 
     def run(self):
         App.run(self)
@@ -191,6 +234,47 @@ class MySettingsWithSidebar(SettingsWithSidebar):
     def on_close(self):
         Logger.info("main.py: MySettingsWithSidebar.on_close")
         self.app.stop()
+
+    def create_popup(self):
+        # create popup layout
+        content = BoxLayout(orientation='vertical', spacing='5dp')
+        popup_width = min(0.95 * Window.width, dp(500))
+        self.popup = popup = Popup(
+            title="password", content=content, size_hint=(None, None),
+            size=(popup_width, '250dp'))
+
+        # create the textinput used for numeric input
+        self.textinput = textinput = TextInput(
+            text="", font_size='24sp', multiline=False,
+            size_hint_y=None, height='42sp')
+        # textinput.bind(on_text_validate=self._validate)
+        self.textinput = textinput
+
+        # construct the content, widget are used as a spacer
+        content.add_widget(Widget())
+        content.add_widget(textinput)
+        content.add_widget(Widget())
+        # content.add_widget(SettingSpacer())
+
+        # 2 buttons are created for accept or cancel the current value
+        btnlayout = BoxLayout(size_hint_y=None, height='50dp', spacing='5dp')
+        btn = Button(text='Ok')
+        btn.bind(on_release=self.on_my_popup_close)
+        btnlayout.add_widget(btn)
+        btn = Button(text='Cancel')
+        btn.bind(on_release=popup.dismiss)
+        btnlayout.add_widget(btn)
+        content.add_widget(btnlayout)
+
+        # all done, open the popup !
+        popup.open()
+
+    def on_my_popup_close(self,instance):
+        password = self.textinput.text
+        print (password)
+        self.popup.dismiss()
+
+
     def on_config_change(self, config, section, key, value):
         Logger.info(
             "main.py: MySettingsWithSidebar.on_config_change: "
@@ -200,6 +284,8 @@ class MySettingsWithSidebar(SettingsWithSidebar):
             if key == 'conf_button':
                 if value == 'button_connect':
                     print("BUTTON PRESSED")
+                    self.create_popup()
+
 
 
 app = MyApp()
---
+++ extensions/iocompython/examples/i-spy/mblks.py
@@ -0,0 +1,91 @@
+from kivy.config import ConfigParser
+
+
+# This JSON defines entries we want to appear in our App configuration screen
+json2 = '''
+[
+    {
+        "type": "options",
+        "title": "role",
+        "desc": "Listen for incoming connections or connect actively?",
+        "section": "My Label",
+        "key": "conf_role",
+        "options": ["SERVER", "CLIENT"]
+    },
+    {
+        "type": "options",
+        "title": "transport",
+        "desc": "What kind of data transport: Secure TLS, plain socket or serial communication?",
+        "section": "My Label",
+        "key": "conf_transport",
+        "options": ["TLS", "SOCKET", "SERIAL"]
+    },
+    {
+        "type": "string",
+        "title": "IP address",
+        "desc": "IP address of the IO device to connect to.",
+        "section": "My Label",
+        "key": "conf_ip"
+    },
+    {
+        "type": "string",
+        "title": "serial port",
+        "desc": "Serial port used to connect to the IO device.",
+        "section": "My Label",
+        "key": "conf_serport"
+    },
+    {
+        "type": "string",
+        "title": "user name",
+        "desc": "Authenticate to the device as (vieview is client)",
+        "section": "client",
+        "key": "conf_user"
+    },
+    {
+        "type": "string",
+        "title": "certificate chain",
+        "desc": "Chain of trust, PEM certificate bundle file (client)",
+        "section": "client",
+        "key": "conf_cert_chain"
+    },
+    {
+        "type": "string",
+        "title": "server certificate",
+        "desc": "Certificate to present to clients (public)",
+        "section": "server",
+        "key": "conf_serv_cert"
+    },
+    {
+        "type": "string",
+        "title": "server key",
+        "desc": "Server's private key (seacret)",
+        "section": "server",
+        "key": "conf_serv_key"
+    },
+    {
+        "type": "buttons",
+        "title": "",
+        "desc": "",
+        "section": "My Label",
+        "key": "conf_button",
+        "buttons": [{"title":"connect","id":"button_connect"}]
+    }
+]
+'''
+
+
+
+class MblkSpy(ConfigParser):
+    #def __init__(self, *args, **kwargs):
+    #    self.image_nr = -1
+
+    def setup_my_panel(self, settings):
+        # config = ConfigParser()
+        self.setdefaults('My Label', {'conf_role': 'CLIENT', 'conf_transport': 'TLS', 'conf_ip': '192.168.1.220', 'conf_serport': 'COM1', 'conf_authentication': 'CLIENT'})
+        self.setdefaults('client', {'conf_user': 'administrator', 'conf_cert_chain': 'bob-bundle.crt'})
+        self.setdefaults('server', {'conf_serv_cert': 'alice.crt', 'conf_serv_key': 'alice.key'})
+        # config.adddefaultsection('client')
+        # config.setdefaults('client', {'conf_cert_chain': 'bob-bundle.crt'})
+        settings.add_json_panel('Ukemi doo', self, data=json2)
+
+
