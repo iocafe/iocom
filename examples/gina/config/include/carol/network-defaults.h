c216950ad72416b24db87cfcd4be2286fdb1bc92
Pekka Lehtikoski <pekka.lehtikoski@gmail.com>
2020-01-31 11:56:37 -0500

    Mbed TLS

--- examples/gina/config/include/carol/network-defaults.h
+++ examples/gina/config/include/carol/network-defaults.h
@@ -1,6 +1,6 @@
 /* This file is gerated by bin-to-c.py script, do not modify. */
 OSAL_C_HEADER_BEGINS
 
-extern const os_char ioapp_network_defaults[394];
+extern const os_char ioapp_network_defaults[398];
 
 OSAL_C_HEADER_ENDS
