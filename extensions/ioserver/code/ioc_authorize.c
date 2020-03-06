4a77986280e3d0dcafe7027159abf638dd00f3e5
Unknown <pekka.lehtikoski@gmail.com>
2020-03-06 05:28:01 -0500

    xx

--- extensions/ioserver/code/ioc_authorize.c
+++ extensions/ioserver/code/ioc_authorize.c
@@ -315,10 +315,7 @@ static osalStatus ioc_authorize_process_block(
                  */
                 if (match && os_strcmp(state->password, "*")) {
                     match = !os_strcmp(state->user->password, state->password);
-                    if (!match) {
-                        /* User device has wrong password
-                         */
-                        osal_debug_error("authorization check: wrong password");
+                    if (!match && state->ncode != IOC_NOTE_NEW_IO_DEVICE) {
                         state->ncode = IOC_NOTE_WRONG_IO_DEVICE_PASSWORD;
                     }
                 }
@@ -328,6 +325,7 @@ static osalStatus ioc_authorize_process_block(
                     state->valid_user = OS_TRUE;
                     flags = 0;
                     if (!os_strcmp(state->privileges, "admin")) flags |= IOC_AUTH_ADMINISTRATOR;
+                    state->ncode = IOC_NOTE_NEW_IO_DEVICE;
                     ioc_add_allowed_network(state->allowed_networks, state->mblk_network_name, flags);
                 }
             }
@@ -511,7 +509,7 @@ static void ioc_authorize_parse_accounts(
     state.checked_user_name = user_name;
     state.mblk_network_name = network_name;
     state.allowed_networks = allowed_networks;
-    state.ncode = IOC_NOTE_NEW_IO_DEVICE;
+    state.ncode = IOC_NOTE_NONE;
 
     s = osal_create_json_indexer(&jindex, config, config_sz, 0); /* HERE WE SHOULD ALLOW ZERO PADDED DATA */
     if (s)
