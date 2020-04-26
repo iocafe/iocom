6a1ec04bae19e6931ea652e805b2ef7ba355bfff
mint pekka <pekka.lehtikoski@gmail.com>
2020-04-26 03:58:01 -0400

    Setting network defaults

--- examples/candy/config/include/espcam/network-defaults.h
+++ examples/candy/config/include/espcam/network-defaults.h
@@ -1,6 +1,6 @@
 /* This file is gerated by bin-to-c.py script, do not modify. */
 OSAL_C_HEADER_BEGINS
 
-extern const os_char ioapp_network_defaults[283];
+extern const os_char ioapp_network_defaults[267];
 
 OSAL_C_HEADER_ENDS
