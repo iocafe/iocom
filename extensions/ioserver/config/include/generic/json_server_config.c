f8ebd83404222584167e0f88c981836fd181a5cf
mint pekka <pekka.lehtikoski@gmail.com>
2020-05-13 20:24:41 -0400

    Cleanup

--- extensions/ioserver/config/include/generic/json_server_config.c
+++
@@ -1,9 +0,0 @@
-/* This file is gerated by generate_c_code.py script, do not modify. */
-#define IOCOM_IOBOARD
-#include "iocom.h"
-#include "config/include/generic/server_signals.h"
-#include "config/include/generic/server_signals-info-mblk.h"
-#include "config/include/generic/server_accounts.h"
-#include "config/include/generic/server_signals.c"
-#include "config/include/generic/server_signals-info-mblk.c"
-#include "config/include/generic/server_accounts.c"
--- extensions/ioserver/config/include/generic/json_server_config.h
+++
@@ -1,4 +0,0 @@
-/* This file is gerated by generate_c_code.py script, do not modify. */
-#include "config/include/generic/server_signals.h"
-#include "config/include/generic/server_signals-info-mblk.h"
-#include "config/include/generic/server_accounts.h"
