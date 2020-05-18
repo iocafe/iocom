a795f16c14c2e45b9519bc43bacb3657ebe5c9ef
mint pekka <pekka.lehtikoski@gmail.com>
2020-05-18 12:25:54 -0400

    Cloud firewall config script

---
+++ scripts/ufw-cloud-firewall.sh
@@ -0,0 +1,13 @@
+sudo ufw default deny incoming
+sudo ufw default allow outgoing
+sudo ufw default allow 22
+sudo ufw default allow 443
+sudo ufw default allow 80
+sudo ufw default allow 5000
+sudo ufw default allow 6364
+sudo ufw default allow 6365
+sudo ufw default allow 6366
+sudo ufw default allow 6367
+sudo ufw default allow 6368
+sudo ufw default allow 6369
+
