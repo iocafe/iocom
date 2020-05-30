d64572f25ea9ad5c7c5bf8782946f6a4477fa322
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2020-05-30 04:49:10 -0400

    xx

--- extensions/iocompython/scripts/start-qtcreator-for-python-debug.sh
+++ extensions/iocompython/scripts/start-qtcreator-for-python-debug.sh
@@ -1,8 +1,8 @@
-export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/coderoot/python/lib
+export LD_LIBRARY_PATH=/coderoot/python/lib
 
 cd /coderoot/python/bin
 . ./debugpython/bin/activate
 
-export PYTHONPATH=/coderoot/bin/linux/debug:/coderoot/python/lib/python3.9
+export PYTHONPATH=/coderoot/bin/linux/debug
 /home/john/Qt/Tools/QtCreator/bin/qtcreator
 
