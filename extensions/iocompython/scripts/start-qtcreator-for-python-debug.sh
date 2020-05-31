246226a2a83ae486b6d6e717fde602f581831315
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2020-05-31 01:02:50 -0400

    Finally Python/C/Kivy debugging works

--- extensions/iocompython/scripts/start-qtcreator-for-python-debug.sh
+++ extensions/iocompython/scripts/start-qtcreator-for-python-debug.sh
@@ -3,6 +3,6 @@ export LD_LIBRARY_PATH=/coderoot/python/lib
 cd /coderoot/python/bin
 . ./debugpython/bin/activate
 
-export PYTHONPATH=/coderoot/bin/linux/debug
+export PYTHONPATH=/coderoot/bin/linux/debug:/coderoot/python/bin/debugpython/lib/python3.7/site-packages
 /home/john/Qt/Tools/QtCreator/bin/qtcreator
 
