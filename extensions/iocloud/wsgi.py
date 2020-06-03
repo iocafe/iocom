f971ad7ac1b98abac2a6f66fa00894d7e18808cb
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2020-06-03 02:00:40 -0400

    xx

--- extensions/iocloud/wsgi.py
+++ extensions/iocloud/wsgi.py
@@ -2,8 +2,8 @@
 # if __name__ == "__main__":
 #     app.run()
     
-from login import create_app
-app = create_app()
+import login 
+app = login.create_app()
 
 if __name__ == "__main__":
     app.run(host='0.0.0.0', debug=True)
