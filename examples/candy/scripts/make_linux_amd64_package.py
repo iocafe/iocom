c0f7f041b0650d6157e96e12a30fe408b26cf4fa
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2020-07-08 10:43:03 -0400

    Scripting "make debian package"

---
+++ examples/candy/scripts/make_linux_amd64_package.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python3
+import os
+import platform
+
+if platform.system() == 'Windows':
+    MYPYTHON = 'python'
+    MYCODEROOT = 'c:/coderoot'
+else:
+    MYPYTHON = 'python3'
+    MYCODEROOT = '/coderoot'
+
+def runcmd(cmd):
+    stream = os.popen(cmd)
+    output = stream.read()
+    print(output)
+
+runcmd(MYPYTHON + ' ' + MYCODEROOT + '/eosal/scripts/make_debian_package.py ' + MYCODEROOT + '/iocom/examples/candy -a candy -s linux -h amd64 -o iocafe -d "Candy camera application"')
+
