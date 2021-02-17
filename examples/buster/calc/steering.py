3fe44cf1c1ff1242a97e05cb6755db75fea8dd90
mint pekka <pekka.lehtikoski@gmail.com>
2021-02-16 21:54:28 -0500

    xx

--- examples/buster/calc/steering.py
+++ examples/buster/calc/steering.py
@@ -20,14 +20,17 @@ def plot_line(plt, x1, y1, x2, y2):
     plt.plot([x1, x2], [y1, y2])
 
 def simulate(plt, myrange):
-    b_wheel_d = 7
-    b_wheel_w = 1.5
-    b_wheel_x = 10
-    f_wheel_d = 4
-    f_wheel_w = 1.3
-    f_wheel_x = 9
-    f_wheel_y = 20
+    b_wheel_d = 9.6
+    # b_wheel_d = (3 + 3/4) *2.54
+    b_wheel_w = 2.3
+    b_wheel_x = 5.5 * 2.54
+    f_wheel_d = 7.6
+    # f_wheel_d = 3 * 2.54
+    f_wheel_w = 2.3
+    f_wheel_x = 4.6 * 2.54
+    f_wheel_y = (7 + 1/2) * 2.54
 
+    # We use randians and degrees to opposite direction
     coeff = -2.0 * 3.1415 / 360
 
     # for steering in range(-91,91,2):
@@ -35,45 +38,61 @@ def simulate(plt, myrange):
         plt.cla()
         plt.text(0, -0.1, str(int(steering)), horizontalalignment='center',verticalalignment='top')
 
-        a = coeff * steering
+        # "steering" input from -90 degrees (right) to 90 degrees (left) is for direction. 0 = straight forward.
+        # "speed" is movement speed from -100% (backwards) to 100% (forward).
+        speed = 100
 
+        # Calculate center_x on back wheel axis as rotation center.
+        a = coeff * steering
         center_x = f_wheel_y / math.tan(a)
 
-        # center_x = 20
+        # Just decoration
+        dx = f_wheel_x - center_x;
+        c = plt.Circle((center_x, 0), radius=math.sqrt(dx*dx + f_wheel_y*f_wheel_y), color='red', alpha=.05)
+        plt.gca().add_artist(c)
+        dx = f_wheel_x + center_x;
+        c = plt.Circle((center_x, 0), radius=math.sqrt(dx*dx + f_wheel_y*f_wheel_y), color='red', alpha=.05)
+        plt.gca().add_artist(c)
 
-        x_space = 6
-
-        plot_line(plt, f_wheel_x, f_wheel_y, center_x, 0)
+        # Calculate wheel directins in radians and convert to degrees
         ar = math.atan((f_wheel_y)/(center_x - f_wheel_x))
         al = math.atan((f_wheel_y)/(center_x + f_wheel_x))
-        plot_line(plt, -f_wheel_x, f_wheel_y, center_x, 0)
+        r_dir = ar/coeff
+        l_dir = al/coeff
 
+        # Plot wheels and lines
+        plot_line(plt, f_wheel_x, f_wheel_y, center_x, 0)
+        plot_line(plt, -f_wheel_x, f_wheel_y, center_x, 0)
+        x_space = 6
         plot_wheel(plt, b_wheel_x, 0, b_wheel_d, b_wheel_w, 0.0)
         plot_wheel(plt, -b_wheel_x, 0, b_wheel_d, b_wheel_w, 0.0)
         plot_line(plt, -b_wheel_x * x_space, 0,  b_wheel_x * x_space, 0)
         plot_wheel(plt, f_wheel_x, f_wheel_y, f_wheel_d, f_wheel_w, ar)
         plot_wheel(plt, -f_wheel_x, f_wheel_y, f_wheel_d, f_wheel_w, al)
 
-        plt.text(f_wheel_x, f_wheel_y - f_wheel_d - 0.1, str(int(ar/coeff)), horizontalalignment='center',verticalalignment='top')
-        plt.text(-f_wheel_x, f_wheel_y - f_wheel_d - 0.1, str(int(al/coeff)), horizontalalignment='center',verticalalignment='top')
+        # Show directions        
+        plt.text(f_wheel_x, f_wheel_y - f_wheel_d - 0.1, str(int(r_dir)), horizontalalignment='center',verticalalignment='top')
+        plt.text(-f_wheel_x, f_wheel_y - f_wheel_d - 0.1, str(int(l_dir)), horizontalalignment='center',verticalalignment='top')
 
+        # Calculate motor speeds
         if center_x > b_wheel_x:
-            sl = 100
+            sl = speed
             sr = sl * (center_x - b_wheel_x) /  (center_x + b_wheel_x)
         elif center_x < -b_wheel_x:
-            sr = 100
+            sr = speed
             sl = sr * (-center_x - b_wheel_x) /  (-center_x + b_wheel_x)
         elif center_x > 0:
-            sl = 100 * (center_x + b_wheel_x) / (2 * b_wheel_x)
+            sl = speed * (center_x + b_wheel_x) / (2 * b_wheel_x)
             sr = -sl * (b_wheel_x - center_x) /(b_wheel_x + center_x)
         else:
-            sr = 100 * (-center_x + b_wheel_x) / (2 * b_wheel_x)
+            sr = speed * (-center_x + b_wheel_x) / (2 * b_wheel_x)
             sl = -sr * (b_wheel_x + center_x) /(b_wheel_x - center_x)
 
+        # Show motor speeds
         plt.text(b_wheel_x, -b_wheel_d - 0.1, str(int(sr)) + "%", horizontalalignment='center',verticalalignment='top')
         plt.text(-b_wheel_x, -b_wheel_d - 0.1, str(int(sl)) + "%", horizontalalignment='center',verticalalignment='top')
 
-        plt.ylabel('some numbers')
+        plt.ylabel('grimpy wheel direction and speed')
         plt.gca().set_aspect('equal', adjustable='box')
         plt.xlim([-50, 50])
         plt.pause(0.05)
