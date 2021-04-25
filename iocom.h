1124bb28ebf76edaa2412763ac545f4eff08f1ac
mint pekka <pekka.lehtikoski@gmail.com>
2021-04-24 20:18:43 -0400

    xx

--- iocom.h
+++ iocom.h
@@ -33,7 +33,7 @@ OSAL_C_HEADER_BEGINS
 /* IOCOM library version number.
  */
 #ifndef IOCOM_VERSION
-#define IOCOM_VERSION "200712"
+#define IOCOM_VERSION "210424"
 #endif
 
 /* Enable/disable dynamic allocation of memory blocks and resizing
