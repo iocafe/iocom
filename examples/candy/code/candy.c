fb842ce7d06fb655bb527a503b156227539b278f
Pekka Lehtikoski <pekka.lehtikoski@iocafe.org>
2021-08-27 04:35:24 -0400

    xx

--- examples/candy/code/candy.c
+++ examples/candy/code/candy.c
@@ -140,6 +140,10 @@ osalStatus osal_main(
     dinfoSystemSpeSignals si_sigs;
     dinfoResMonSignals rm_sigs;
 
+    /* Allow a moment to power up (capacitors, etc)
+     */
+    os_sleep(500);
+
     /* Setup error handling. Here we select to keep track of network state. We could also
        set application specific event handler callback by calling osal_set_net_event_handler().
      */
